6943879760def6ef0fd9301a4e94e26f1c5c2532
DaisyChepyegon <daisychepyegon@gmail.com>
2022-09-07 17:07:54 +0300

    create the signup page

--- quick_rental_frontend/src/Signup/Signup.js
+++ quick_rental_frontend/src/Signup/Signup.js
@@ -1,9 +1,41 @@
 import React from 'react'
 
 function Signup() {
+  
+
   return (
     <div>
       
+      <div className="signup">
+      <div className="imgs">
+      <img src={home} alt="image0" height={550} width={500}/>
+      </div>
+      <div className="frm">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit} className="sign">
+        
+        <label> Name</label>
+        <input type="text" placeholder="Name" 
+          value={name}
+          required
+          onChange={e => setName ( e.target.value)}
+        />
+        <label>Email </label>
+        <input type="email" placeholder="Email"
+          value={email}
+          required
+          onChange={e => setEmail( e.target.value)}
+        />
+        <label>Password</label>
+        <input type="password" placeholder="Password"
+          value={password}
+          required
+           onChange={e => setPassword( e.target.value)}
+         />
+        <button >Sign Up</button>
+        </form>
+        </div>
+      </div>
     </div>
   )
 }
