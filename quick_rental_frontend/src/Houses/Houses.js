a518f4d783a92192f1b300a0497f4069617666b1
DaisyChepyegon <daisychepyegon@gmail.com>
2022-09-09 12:10:51 +0300

    display data to the page

--- quick_rental_frontend/src/Houses/Houses.js
+++ quick_rental_frontend/src/Houses/Houses.js
@@ -8,12 +8,29 @@ function Houses() {
     .then((resp) => resp.json())
     .then((houses) => setHouses(houses));
   },[]);
-  console.log(houses)
+  console.log(houses);
+
+  let container = (
+    houses.map((house) => (
+      <div>
+        <div className='cont'>
+          <p> {house.location} </p>
+          <p> {house.price} </p>
+          <p> {house.bedrooms} </p>
+          <p> {house.description} </p>
+        </div>
+      </div>
+    ))
+  )
+
+
   return (
     <div>
-      
+      <div>{container}</div>
     </div>
+    
   )
+  
 }
 
 export default Houses
