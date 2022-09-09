74733987adfa54dc6c2c90d896a500ee10796d45
DaisyChepyegon <daisychepyegon@gmail.com>
2022-09-09 12:02:01 +0300

    fetch data from backend using useEffect

--- quick_rental_frontend/src/Houses/Houses.js
+++ quick_rental_frontend/src/Houses/Houses.js
@@ -3,7 +3,12 @@ import React,{useEffect,useState} from 'react'
 function Houses() {
   const[houses, setHouses] = useState([])
 
-  
+  useEffect(() => {
+    fetch("http://localhost:9292/houses")
+    .then((resp) => resp.json())
+    .then((houses) => setHouses(houses));
+  },[]);
+  console.log(houses)
   return (
     <div>
       
