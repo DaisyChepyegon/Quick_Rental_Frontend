228a82558f15f68c4fa330021e328f55acbd9252
DaisyChepyegon <daisychepyegon@gmail.com>
2022-09-10 13:27:47 +0300

    add forms

--- quick_rental_frontend/src/Houses/HouseForms.js
+++ quick_rental_frontend/src/Houses/HouseForms.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function HouseForms() {
+function HouseForms({addHouse}) {
+
+  const [formData, setFormData] = useState(
+    {
+      location:"",
+      bedrooms:"",
+      price:"",
+      description:""
+    })
+
+    function handleChange(event){
+      const name = event.target.name
+      let value = event.target.value
+
+      setFormData({
+        ...formData,
+        [name]:value,
+      })
+    }
   return (
-    <div>
-      
-    </div>
+   <form>
+     <h2>Houses</h2>
+     <input
+      name='location'
+      type="text"
+      placeholder='location'
+      onChange={handleChange}
+      value={formData.location}
+      />
+
+     <input
+      name='bedrooms'
+      type="integer"
+      placeholder='bedrooms'
+      onChange={handleChange}
+      value={formData.bedrooms}
+      />
+
+      <input
+      name='price'
+      type="integer"
+      placeholder='price'
+      onChange={handleChange}
+      value={formData.price}
+      />
+
+     <textarea 
+      name="descriprion"
+      placeholder="Write your description here..." rows={6}
+      onChange={handleChange}
+      value={formData.description}
+     />
+
+     <input className='btn' type="submit" value="send" />
+
+   </form>
   )
 }
 
