311dd9de70fb51ad71c68a00f47eabbe377a1313
DaisyChepyegon <daisychepyegon@gmail.com>
2022-09-12 15:08:14 +0300

    add event listener to filter locations in search button

--- quick_rental_frontend/src/Search/Search.js
+++ quick_rental_frontend/src/Search/Search.js
@@ -5,17 +5,25 @@ import './search.css'
 function Search({placeholder, data}) {
   const[filterHouses, SetFilterHouses] = useState([])
 
+  const handleFilter = (event) =>{
+    const searchword = event.target.value
+    const newFilter = data.filter((value) => {
+      return value.location.includes(searchword)
+    })
+    SetFilterHouses(newFilter)
+  }
+
   return (
     <div className='search'>
       <div className='inputs'>
-        <input type="text" placeholder={placeholder} onChange={}/>
+        <input type="text" placeholder={placeholder} onChange={handleFilter}/>
         <div className='icon'>
           <FaSearchengin />
         </div>
       </div>
       { filterHouses.length !=0 && (
       <div className='results'>
-        {data.map((value, key) =>{
+        {filterHouses.map((value, key) =>{
           return (
             <a className='dataitem' href={value.link} target="_parent">
           <p>{value.location}</p>
